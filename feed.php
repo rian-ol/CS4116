45000bab41755916f132b3a19a34d158c1cff07b
rian-ol <64734797+rian-ol@users.noreply.github.com>
2023-04-22 12:37:03 +0100

    Update feed.php
    Added cookie check

--- feed.php
+++ feed.php
@@ -1,3 +1,12 @@
+<?php
+
+  if(!isset($_COOKIE['user'])){
+    header('Location:index.php');
+    exit;
+  }
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -31,8 +40,6 @@
               <div class="card shadow-0">
                 <div class="card-body border-bottom pb-2">
                   <div class="d-flex">
-                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img (31).webp" class="rounded-circle"
-                      height="50" alt="Avatar" loading="lazy" />
                     <div class="d-flex align-items-center w-100 ps-3">
                       <div class="w-100">
                         <input type="text" id="form143" class="form-control form-status border-0 py-1 px-0"
@@ -59,15 +66,13 @@
                       </li>
                     </ul>
                     <div class="d-flex align-items-center">
-                      <button type="button" class="btn btn-primary btn-rounded">Tweet</button>
+                      <button type="button" class="btn btn-primary btn-rounded">Post</button>
                     </div>
                   </div>
                 </div>
               </div>
               <div>
                 <div class="d-flex p-3 border-bottom">
-                  <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img (29).webp" class="rounded-circle"
-                    height="50" alt="Avatar" loading="lazy" />
                   <div class="d-flex w-100 ps-3">
                     <div>
                       <a href="">
@@ -83,22 +88,10 @@
                         Some random text absolutely random <a href="">#mysterylink</a> more text.
                         Text text texty text. Text text texty text Text text texty text. Text text texty text
                       </p>
-                      <ul class="list-unstyled d-flex justify-content-between mb-0 pe-xl-5">
-                        <li>
-                          <i class="far fa-comment"></i>
-                        </li>
-                        <li><i class="fas fa-retweet"></i><span class="small ps-2">7</span></li>
-                        <li><i class="far fa-heart"></i><span class="small ps-2">35</span></li>
-                        <li>
-                          <i class="far fa-share-square"></i>
-                        </li>
-                      </ul>
                     </div>
                   </div>
                 </div>
                 <div class="d-flex p-3 border-bottom">
-                  <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img (29).webp" class="rounded-circle"
-                    height="50" alt="Avatar" loading="lazy" />
                   <div class="d-flex w-100 ps-3">
                     <div>
                       <a href="#">
@@ -116,37 +109,14 @@
                       <div class="card border mb-3 shadow-0" style="max-width: 540px;">
                         <div class="row g-0">
                           <div class="col-md-3">
-                            <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img (21).webp" alt="Avatar"
-                              class="img-fluid rounded-left" />
                           </div>
-                          <div class="col-md-9">
-                            <div class="card-body">
-                              <p class="card-text" style="line-height: 1;">
-                                Stuff
-                              </p>
-                              <p class="card-text small mb-0" style="line-height: 1.2;">
-                                Data.
-                              </p>
-                            </div>
                           </div>
                         </div>
                       </div>
-                      <ul class="list-unstyled d-flex justify-content-between mb-0 pe-xl-5">
-                        <li>
-                          <i class="far fa-comment"></i>
-                        </li>
-                        <li><i class="fas fa-retweet"></i><span class="small ps-2">51</span></li>
-                        <li><i class="far fa-heart"></i><span class="small ps-2">185</span></li>
-                        <li>
-                          <i class="far fa-share-square"></i>
-                        </li>
-                      </ul>
                     </div>
                   </div>
                 </div>
                 <div class="d-flex p-3 border-bottom">
-                  <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img (22).webp" class="rounded-circle"
-                    height="50" alt="Avatar" loading="lazy" />
                   <div class="d-flex w-100 ps-3">
                     <div>
                       <a href="#">
@@ -165,69 +135,13 @@
                       <div class="card border mb-3 shadow-0" style="max-width: 540px;">
                         <div class="row g-0">
                           <div class="col-md-3">
-                            <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img (20).webp" alt="Avatar"
-                              class="img-fluid rounded-left" />
-                          </div>
-                          <div class="col-md-9">
-                            <div class="card-body">
-                              <p class="card-text" style="line-height: 1;">
-                                Title of the news
-                              </p>
-                              <p class="card-text small mb-0" style="line-height: 1.2;">
-                               News.
-                              </p>
-                            </div>
                           </div>
                         </div>
                       </div>
-                      <ul class="list-unstyled d-flex justify-content-between mb-0 pe-xl-5">
-                        <li>
-                          <i class="far fa-comment"></i>
-                        </li>
-                        <li><i class="fas fa-retweet"></i><span class="small ps-2">8</span></li>
-                        <li><i class="far fa-heart"></i><span class="small ps-2">97</span></li>
-                        <li>
-                          <i class="far fa-share-square"></i>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-                <div class="d-flex p-3 border-bottom">
-                  <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img (24).webp" class="rounded-circle"
-                    height="50" alt="Avatar" loading="lazy" />
-                  <div class="d-flex w-100 ps-3">
-                    <div>
-                      <a href="">
-                        <h6 class="text-body">
-                          Anna Doe
-                          <span class="small text-muted font-weight-normal">@annadoe</span>
-                          <span class="small text-muted font-weight-normal"> • </span>
-                          <span class="small text-muted font-weight-normal">7h</span>
-                          <span><i class="fas fa-angle-down float-end"></i></span>
-                        </h6>
-                      </a>
-                      <p style="line-height: 1.2;">
-                        Heres an image.
-                      </p>
-                      <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp"
-                        class="img-fluid rounded mb-3" alt="Fissure in Sandstone" />
-                      <ul class="list-unstyled d-flex justify-content-between mb-0 pe-xl-5">
-                        <li>
-                          <i class="far fa-comment"></i>
-                        </li>
-                        <li><i class="fas fa-retweet"></i><span class="small ps-2">11</span></li>
-                        <li><i class="far fa-heart"></i><span class="small ps-2">65</span></li>
-                        <li>
-                          <i class="far fa-share-square"></i>
-                        </li>
-                      </ul>
                     </div>
                   </div>
                 </div>
                 <div class="d-flex p-3 border-bottom mb-5">
-                  <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img (3).webp" class="rounded-circle"
-                    height="50" alt="Avatar" loading="lazy" />
                   <div class="d-flex w-100 ps-3">
                     <div>
                       <a href="">
@@ -243,20 +157,6 @@
                         Some random text absolutely random <a href="">#mysterylink</a> more text.
                         Text text texty text. Text text texty text Text text texty text. Text text texty text and heres a video.
                       </p>
-                      <div class="ratio ratio-16x9 mb-3">
-                        <iframe src="https://www.youtube.com/embed/vlDzYIIOYmM" title="YouTube video"
-                          allowfullscreen></iframe>
-                      </div>
-                      <ul class="list-unstyled d-flex justify-content-between mb-0 pe-xl-5">
-                        <li>
-                          <i class="far fa-comment"></i>
-                        </li>
-                        <li><i class="fas fa-retweet"></i><span class="small ps-2">34</span></li>
-                        <li><i class="far fa-heart"></i><span class="small ps-2">159</span></li>
-                        <li>
-                          <i class="far fa-share-square"></i>
-                        </li>
-                      </ul>
                     </div>
                   </div>
                 </div>
