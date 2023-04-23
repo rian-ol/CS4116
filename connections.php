aec4c23b0f2b2ed8ad7e3b99681bd6551cdd9fd0
rian-ol <64734797+rian-ol@users.noreply.github.com>
2023-04-23 21:58:20 +0100

    Update connections.php
    Added functionality to display users in the database

--- connections.php
+++ connections.php
@@ -3,12 +3,38 @@
   if(!isset($_COOKIE['user'])){
     header('Location:index.php');
     exit;
-  }
+  } 
+
+ 
+
+  include('connection.php');
+    $sql = "SELECT u.User_id, u.First_Name, u.Surname, p.Age, p.Gender, p.Location, p.skill_name FROM user u Join profile p ON u.User_id = p.User_id;";  
+    $result = mysqli_query($con, $sql);  
+
+    $Connects = array();
+    while ($row = mysqli_fetch_assoc($result)) {
+      if ($row['User_id'] == $_COOKIE['user']) {
+        continue;
+      }
+      $key = $row['First_Name'] . $row ['Surname'];
+      if(!array_key_exists($key, $Connects)){
+        $Connects[$key] = array(
+          'User_id' => $row['User_id'],
+          'First_Name'=> $row['First_Name'],
+          'Surname'=> $row['Surname'],
+          'Age'=> $row['Age'],
+          'Gender'=> $row['Gender'],
+          'Location'=> $row['Location'],
+          'skill_name'=> $row['skill_name'],
+        );
+      }
+    }
+    
+    
 
 ?>
 
-<!DOCTYPE html>
-<html>
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -34,5 +60,37 @@
             </form>
            </div>
          </div>
+
+        <?php
+        $int = 0;
+        ?>
+          <div class="limit4"></div>
+        <?php
+        foreach ($Connects as $connect) {
+          if ($int % 4 == 0) {
+            ?>
+            <div class="limit4"></div>
+              <?php
+          }
+          ?>
+            <div class="pot-connect">  
+              <h2> <?php echo $connect['First_Name']. " " . $connect['Surname']; ?></h2>
+              <p>Age: <?php echo $connect['Age']; ?></p>
+              <p>Gender: <?php echo $connect['Gender']; ?></p>
+              <p>Location: <?php echo $connect['Location']; ?></p>
+              <p>Skill: <?php echo $connect['skill_name']; ?></p>
+              <form action="connectUser.php">
+              <div ><!--<a href="connectUser.php"> --><button class="btn btn-primary profile-button " class="button" name="user_id" type="submit" value="<?php echo $connect['User_id']; ?>">Connect with user</button><!--</a>--></div>  
+              </form>
+            </div>
+          </div>
+                  <?php
+                  $int++;
+                }
+                  ?>
+
+
+
+
 </body>
 </html>
