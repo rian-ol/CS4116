df2d7a88773737c9dc4c680a5c44e487c69038eb
rian-ol <64734797+rian-ol@users.noreply.github.com>
2023-04-25 05:08:41 +0100

    Updated create vacancy method

--- createVacancyMethod.php
+++ createVacancyMethod.php
@@ -1,26 +1,33 @@
 <?php
 include('connection.php');  
+        $userid = $_COOKIE['user'];
         $Vacancy_name =  $_REQUEST['Vacancy_name'];
         $Vacancy_description = $_REQUEST['Vacancy_description'];
-        $Experienced_required =  $_REQUEST['Experienced_required'];
+        // $Experienced_required =  $_GET['Experienced_required'];
         $Location = $_REQUEST['Location'];
+        $skill_name = $_REQUEST['skill_name'];
+
+        $sql = "SELECT * FROM organisation WHERE  User_id= '$userid'";
+        $result = mysqli_query($con, $sql);  
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+        $organisation_id = $row['organisation_id'];
+            
+            
  
          
-        // Performing insert query execution
-        // here our table name is user
-        $sql = "INSERT INTO Vacancy (Vacancy_name, Vacancy_description, Experienced_required, Location) 
-        VALUES ('$Vacancy_name', '$Vacancy_description', '$Experienced_required','$Location')";
-         
-        if(mysqli_query($con, $sql)){
-            header("Location:vacancies.php");
- 
-           
-        } else{
-            echo "ERROR: Hush! Sorry $sql. "
-                . mysqli_error($con);
-        }
-         
+            $sql = "INSERT INTO Vacancy (Vacancy_name, Vacancy_description, Location, skill_name, organisation_id) 
+            VALUES ('$Vacancy_name', '$Vacancy_description','$Location', '$skill_name', '$organisation_id')";
+            
+            if(mysqli_query($con, $sql)){
+                header("Location:myCompany.php");
+    
+            
+            } else{
+                echo "ERROR: Hush! Sorry $sql. "
+                    . mysqli_error($con);
+            }
+        
         // Close connection
         mysqli_close($con);
         ?>
-    
